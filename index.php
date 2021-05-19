543a553c7124745d8da4ed43a44c231aa3c92e59
Michele Lorusso <kirito102095@hotmail.it>
2021-05-19 15:28:43 +0200

    Header End

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+    $link = ['Introduzione', 'Norme sulla Privacy', 'Termini di Servizio', 'Tecnologie', 'Domande Frequenti'];
    
 ?>
 
@@ -14,8 +16,48 @@
     <!-- CSS -->
     <link rel="stylesheet" href="css/style.css">
 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
+
+    <!-- HEADER -->
+    <header>
+
+        <!-- Logo -->
+        <div class="logo">
+
+            <div class="privacy"><img src="img/google.png" alt="google"><span> Privacy & Terms</span></div>
+
+            <div class="account">
+                <svg fill="#5f6368" class="gb_We" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg>
+                <img src="img/google-plus-512.png" alt="account">
+            </div>
+    
+        </div>
+        <!-- End Logo -->
+        
+        <!-- Link-bar -->
+        <div class="link-bar">
+            <ul>
+                <?php foreach($link as $li) {?>
+
+                    <li><?php echo($li)?></li>
+                    
+                <?php } ?>
+            </ul>
+        </div>
+        <!-- End Link-bar -->
+
+    </header>
+    <!-- END HEADER -->
+
+    <!-- Main -->
+    <main>
+    </main>
+    <!-- END Main -->
+
     
 </body>
 </html>
